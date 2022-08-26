3c3054148a7bd015f0cca0ae31efb53bd8ad1920
Dmitry <cbuilder@users.noreply.github.com>
2022-08-26 13:41:53 +0300

    Build kernel for Cortex-M33 with FPU, Cortex-M33 without FPU (for Elvees ELIoT core0). M4 and M7 directories now marked with f to distinguish.

---
+++ cores/production/standard-cortex-m33/standard-cortex-m33.c
@@ -0,0 +1,13 @@
+/** 
+  ******************************************************************************
+  *  @file   standard-cortex-m33.c
+  *  @brief  Kernel dependencies root.
+  ******************************************************************************
+  *  Copyright (C) JSC EREMEX, 2008-2022.
+  *  $$LICENSE$
+  *****************************************************************************/
+
+#include FX_INTERFACE(FXRTOS)
+
+FX_METADATA(({ implementation: [FXRTOS, STANDARD_CORTEX_M33] }))
+
